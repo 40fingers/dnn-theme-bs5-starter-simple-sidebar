95b1983e88a64a4510c2bedfa6554befc4911a8b
Timo Breumelhof <timo@40fingers.net>
2024-05-28 13:29:29 +0200

    version 01.01.00 sync with Community Theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
+const { parallel } = require('gulp');
+const concat = require('gulp-concat');
 const clean = require('gulp-clean');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+
+
 
 const folderPath = __dirname;
 const folderName =  folderPath.substring(folderPath.lastIndexOf('\\') + 1); 
@@ -11,14 +15,14 @@ const folderName =  folderPath.substring(folderPath.lastIndexOf('\\') + 1);
 
 // SCSS ----------------------
 const skinScssPath = ['./_src/skin.scss'];
-const scssWatchPath = ['./_src/**/*.scss'];
+const scssWatchPath = ['./_src/**/*.scss','./opencontent/templates/**/*.scss'];
 
-function buildScss() { // Parse only the Skin.less file
-  // 1. What less files to parse?
+function buildScss() { // Parse only the Skin.scss file
+  // 1. What SCSS  files to parse?
   return gulp.src(skinScssPath)
   
  
-   // 2. Parse Less
+   // 2. Parse SCSS 
    .pipe(sass.sync().on('error', sass.logError))  
    
    // 3. Write File
@@ -40,7 +44,7 @@ const containerSource = ['_src/containers/**/*.*'];
 const containersTarget = ['../../containers/' + folderName];
 
 
-function copyContainers() { // Parse only the Skin.less file
+function copyContainers() { 
 
   // 1. clean the DNN Container folder
   gulp.src(containersTarget + '**/*.*', {read: false})
@@ -54,11 +58,50 @@ function copyContainers() { // Parse only the Skin.less file
 	;
 }
 
+const jsWatchPath = '_src/theme/js/**/*.js'
+const jsSource = ['_src/theme/js/menu.js', '_src/theme/js/menu-hover.js', '_src/theme/js/mark-empty-panes.js', '_src/theme/js/scroll-class.js'];
+const jsDest = ['vendors/theme/js'];
 
+function jsBundle(cb){
+
+  gulp.src(jsSource)
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest(jsDest));
+
+  cb();
+
+}
 
 
 function styleTask(){
 	buildScss();
+}
+
+function getVendors(cb){
+
+  // FontAwesome 5 CSS
+  const faCSS = ['node_modules/@fortawesome/fontawesome-free/css/all.css', '_src/theme/_extra/FontAwesome-4-shim/v4-shims.min.css']; // Unfortunately the v4 Shim is not available through npm
+  const faCSSDest = 'vendors/fontawesome/css';
+
+  gulp.src(faCSS)
+  .pipe(gulp.dest(faCSSDest));
+
+    // FontAwesome 5 Fonts
+    const faFonts = ['node_modules/@fortawesome/fontawesome-free/webfonts/*'];
+    const faFontsDest = 'vendors/fontawesome/webfonts';
+  
+    gulp.src(faFonts)
+    .pipe(gulp.dest(faFontsDest));
+
+    // Bootstrap JS
+    const bootsJS = ['node_modules/bootstrap/dist/js/*'];
+    const bootsJSDest = 'vendors/bootstrap/js';
+  
+    gulp.src(bootsJS)
+    .pipe(gulp.dest(bootsJSDest));
+
+  cb();
+
 }
 
 
@@ -67,11 +110,17 @@ function styleTask(){
 function watchTask(){
     gulp.watch(containerSource, { delay: 1000 }, copyContainers);
     gulp.watch(scssWatchPath, buildScss);
+    gulp.watch(jsWatchPath, jsBundle);
 }
+
 
 exports.copyContainers = copyContainers;
 exports.buildScss = buildScss;
 exports.style = styleTask;
+exports.jsbundle = jsBundle;
+exports.vendors = getVendors;
+
+exports.setup = parallel(getVendors, copyContainers, buildScss);
 
 exports.default = watchTask;
 
